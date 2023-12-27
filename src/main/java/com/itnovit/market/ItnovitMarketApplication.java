3b8b78252db6f2bdea3349c551e1f90a42c484be
Jorge A Dominguez M <dominguezjorgea@gmail.com>
2023-12-26 20:09:12 -0500

    init project

---
+++ src/main/java/com/itnovit/market/ItnovitMarketApplication.java
@@ -0,0 +1,13 @@
+package com.itnovit.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItnovitMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItnovitMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itnovit/market/ItnovitMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itnovit.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItnovitMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
